b5959a42375a2db76d42c5d7d699536f08d2b17e
Vinicius Pereira <vguard.2003@gmail.com>
2023-01-27 19:37:38 -0300

    Created about with button for downloading CV

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Info = () => {
+    return(
+        <div className="about__info grid">
+            <div className="about__box">
+                <i class="bx bx-award about__icon" ></i>
+                <h3 className="about__title">experiência</h3>
+                <span className="about__subtitle">3 Anos de trabalho</span>
+            </div>
+
+            <div className="about__box">
+                <i class='bx bxs-coin-stack about__icon'></i>
+                <h3 className="about__title">Projetos</h3>
+                <span className="about__subtitle">+ 30</span>
+            </div>
+
+            <div className="about__box">
+                <i class='bx bxs-castle about__icon'></i>
+                <h3 className="about__title">Completados</h3>
+                <span className="about__subtitle">3 + Projetos</span>
+            </div>
+        </div>
+    )
+}
+
+export default Info
